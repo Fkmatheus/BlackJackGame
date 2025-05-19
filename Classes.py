ca0e33244fcb74a73324b880bb401d34c7badcb8
Matheus Santos D S <fkmatheus.profissional@gmail.com>
2025-05-19 20:50:32 -0300

    Inserindo funções da gameplay

--- Classes.py
+++ Classes.py
@@ -24,6 +24,90 @@ values = {
 
 playing = True
 
+
+def take_bet(chips):
+    while True:
+        try:
+            chips.bet = int(input("How many chips would you like to bet? "))
+        except:
+            print("Sorry, please provide an integer")
+        else:
+            if chips.bet > chips.total:
+                print(f"Sorry, do you not have enough chips! You have: {chips.total}")
+            else:
+                break
+
+def hit(deck, hand):
+
+    single_card = deck.deal()
+    hand.add_card(single_card)
+    hand.adjust_for_ace()
+
+
+def hit_or_stand(deck,hand):
+    global playing
+
+    while True:
+        x = input('Hit or Stand? h or s ')
+
+        if x[0].lower() == 'h':
+            hit(deck,hand)
+        
+        elif x[0].lower() == 's':
+            print("Player Stands. Dealer's Turn")
+            playing = False
+        
+        else:
+            print("Please enter h or s only!")
+            continue
+
+        break
+
+def show_some(player, dealer):
+    print("\n Dealer's Hand: ")
+    print("First card hidden!")
+    print(dealer.cards[1])
+
+    print("\n Player's Hand ")
+    for card in player.cards:
+        print(card)
+
+
+def show_all(player, dealer):
+
+    print("\n Dealer's Hand ")
+    for card in dealer.cards:
+        print(card)
+    
+    print(f"Value of Dealer's hand is: {dealer.value}")
+
+    print("\n Player's Hand ")
+    for card in player.cards:
+        print(card)
+    
+    print(f"Value of Player's hand is: {player.value}")
+
+
+def player_busts(player, dealer, chips):
+    print("BUST Player!")
+    chips.lose_bet()
+
+def player_wins(player, dealer, chips):
+    print("Player WINS!")
+    chips.win_bet()
+
+def dealer_busts(player, dealer, chips):
+    print("Player WINS! DEALER BUSTED!")
+    chips.win_bet()
+
+def dealer_wins(player, dealer, chips):
+    print("DEALER WINS!")
+    chips.lose_bet()
+
+def push(player, dealer):
+    print("Dealer and Player Tie, PUSH! ")
+
+
 class Card:
     def __init__(self,suit,rank):
         self.suit = suit
